8418b4c844a4b5272b510dc6c3b0207c65d01b51
soldiersnake <mariano.maciasgandulfo@gmail.com>
2025-08-06 01:57:53 +0200

    validaciones

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // config de las validaciones del paquete "npm install class-validator class-transformer"
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
   app.enableCors(); //config de CORS
-  
+
 
   await app.listen(process.env.PORT ?? 3000);
 }
