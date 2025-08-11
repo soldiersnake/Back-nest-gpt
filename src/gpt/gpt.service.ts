cd156dbe7cd9f8bb462728426541807b66e0f12d
soldiersnake <mariano.maciasgandulfo@gmail.com>
2025-08-12 00:32:38 +0200

    generacion de back orthography

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { orthographyCheckUseCase } from './use-cases';
 import { OrthographyDto } from './dtos';
+import OpenAI from 'openai';
 
 @Injectable()
 export class GptService {
 
+    private openia = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY
+    })
+
     // El servicio solo va a llamar casos de uso
     async orthographyCheck(orthographyDto: OrthographyDto) {
-        return await orthographyCheckUseCase({
+        return await orthographyCheckUseCase(this.openia, {
             prompt: orthographyDto.prompt
         });
     }
--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,13 +1,52 @@
+import OpenAI from "openai";
+
 interface Options {
     prompt: string;
 }
 
-export const orthographyCheckUseCase = async (option: Options) => {
+export const orthographyCheckUseCase = async (openia: OpenAI, option: Options) => {
 
     const { prompt } = option;
 
-    return {
-        prompt: prompt,
-        apiKey: process.env.OPENAI_API_KEY
+    try {
+        const completion = await openia.chat.completions.create({
+            messages: [
+                {
+                    role: "system",
+                    content: `
+                    Te serán proveído textos con posibles errores orográficos y gramaticales,
+                    las palabras deben de exitir en el diccionario de la Real Academia Española, debes de responder en formato JSON válido, sin texto antes o después, sin bloques de código \`\`\`.
+                    tu tarea es corregirlos y retornar información soluciones, también debes de dar porcentaje de acierto por el usuario,
+    
+                    Si no hay errores, debes de retornar un mensaje de felicitaciones.
+
+                    Ejemoplo de salida:
+                    {
+                        "UserScore": number,
+                        "Errors": string[],  // ["error  -> solucion"],
+                        "Message": string // Usa emojis y textos para felicitar al usuario
+                    }
+                    `,
+                },
+                {
+                    role: 'user',
+                    content: prompt
+                },
+            ],
+            model: "gpt-4o",
+            temperature: 0.3, // entre mas cercano a 0 es mas preciso y menos randon las respuesta
+            max_tokens: 150 // es el costo que tendra la respuesta
+        });
+        console.log(completion);
+
+        // si no existe contenido enviamos un Obj vacio para que no llegue un NULL
+        const content = completion.choices[0].message.content ?? "{}";
+        // Elimina bloques de código markdown si existen
+        const cleaned = content.replace(/```json\s*|\s*```/g, "").trim();
+        const jsonResp = JSON.parse(cleaned);
+        return jsonResp;
+    } catch (error) {
+        console.log('ERROR MENSAJE', error);
+        return error.error.message;
     }
 }
